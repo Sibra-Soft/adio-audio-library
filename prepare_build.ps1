21cc26023bb63d29ef472db735b709c72227f551
Alex van den Berg <alex@sibra-soft.nl>
2026-02-02 17:35:02 +0100

    Replace batch scripts with PowerShell versions

---
+++ prepare_build.ps1
@@ -0,0 +1,43 @@
+# Stop bij fouten
+$ErrorActionPreference = "Stop"
+
+# Configuration
+$PROJECT_DIR = "./Source/"
+$COPY_LIST   = Join-Path $PROJECT_DIR "resources\build-files.txt"
+
+Set-Location $PROJECT_DIR
+
+# Pre-Build
+Write-Host "=== Prepare-Build ==="
+
+if (-not (Test-Path $COPY_LIST)) {
+    Write-Error "$COPY_LIST not found"
+    exit 1
+}
+
+Get-Content $COPY_LIST | ForEach-Object {
+    if ([string]::IsNullOrWhiteSpace($_)) { return }
+
+    # Split on ;
+    $parts = $_ -split ";", 2
+    if ($parts.Count -ne 2) { return }
+
+    $SRC = $parts[0].Trim()
+    $DST = $parts[1].Trim()
+
+    Write-Host "Copying $SRC"
+
+    if (-not (Test-Path $DST)) {
+        New-Item -ItemType Directory -Path $DST -Force | Out-Null
+    }
+
+    Copy-Item `
+        -Path $SRC `
+        -Destination $DST `
+        -Recurse `
+        -Force `
+        -ErrorAction Stop
+}
+
+Write-Host "=== Prepare-Build Completed ==="
+exit 0
---
+++ prepare_test.ps1
@@ -0,0 +1,68 @@
+# Stop bij fouten
+$ErrorActionPreference = "Stop"
+
+# Configuration
+$PROJECT_DIR = "./Tests/"
+$COPY_LIST   = Join-Path $PROJECT_DIR "resources\build-files.txt"
+
+function Register($file)
+{
+	$Regsvr32_32 = Join-Path $env:WINDIR "SysWOW64\regsvr32.exe"
+	$Regsvr32_64 = Join-Path $env:WINDIR "System32\regsvr32.exe"
+
+    $useRegsvr32 = $Regsvr32_64
+    if ($filePath -match '\\SysWOW64\\') {
+        $useRegsvr32 = $Regsvr32_32
+    }
+	
+	Write-Host "Dependency: $file"
+    $p = Start-Process -FilePath $useRegsvr32 -ArgumentList "/s", "`"$file`"" -Wait -PassThru
+
+    if ($p.ExitCode -ne 0) {
+        Write-Host "  [ERROR] regsvr32 exitcode = $($p.ExitCode)" -ForegroundColor Red
+    }
+    else {
+        Write-Host "  [OK]" -ForegroundColor Green
+    }
+}
+
+Set-Location $PROJECT_DIR
+
+# Pre-Build
+Write-Host "=== Prepare-Test ==="
+
+if (-not (Test-Path $COPY_LIST)) {
+    Write-Error "$COPY_LIST not found"
+    exit 1
+}
+
+Get-Content $COPY_LIST | ForEach-Object {
+    if ([string]::IsNullOrWhiteSpace($_)) { return }
+
+    # Split on ;
+    $parts = $_ -split ";", 2
+    if ($parts.Count -ne 2) { return }
+
+    $SRC = $parts[0].Trim()
+    $DST = $parts[1].Trim()
+
+    Write-Host "Copying $SRC"
+
+    if (-not (Test-Path $DST)) {
+        New-Item -ItemType Directory -Path $DST -Force | Out-Null
+    }
+
+    Copy-Item `
+        -Path $SRC `
+        -Destination $DST `
+        -Recurse `
+        -Force `
+        -ErrorAction Stop
+}
+
+Set-Location $PROJECT_DIR
+
+Register("../Build/midiio32.ocx")
+
+Write-Host "=== Prepare-Test Completed ==="
+exit 0
